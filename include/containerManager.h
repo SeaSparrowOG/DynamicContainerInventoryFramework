0a9bb33bd507c8569e132e4c69e960059c6822b6
SeaSparrow <contactMeOnNexus@gmail.com>
2024-05-26 20:58:49 +0300

    Fixed removeKeywords not starting out empty

--- include/containerManager.h
+++ include/containerManager.h
@@ -4,7 +4,7 @@ namespace ContainerManager {
 	struct SwapRule {
 		int                              count               { 1 };
 		std::string                      ruleName            { std::string() };
-		std::vector<std::string>         removeKeywords      { std::string() };
+		std::vector<std::string>         removeKeywords      { };
 		RE::TESBoundObject*              oldForm             { nullptr };
 		std::vector<std::string>         locationKeywords    { std::vector<std::string>() };
 		std::vector<RE::TESObjectCONT*>  container           { std::vector<RE::TESObjectCONT*>() };
