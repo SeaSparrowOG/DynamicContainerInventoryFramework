01814273f3b059162b066b361f6a8f255ff0b2bc
SeaSparrow <seasparrow>
2024-07-13 20:54:31 +0300

    Added safeguard against circular leveled lists

--- src/containerCache.cpp
+++ src/containerCache.cpp
@@ -1,13 +1,32 @@
 #include "containerCache.h"
 
 namespace {
-	void ResolveContainerLeveledList(RE::TESLeveledList* a_list, std::vector<RE::TESBoundObject*>* a_result) {
+	void ResolveContainerLeveledList(RE::TESLeveledList* a_list, std::vector<RE::TESBoundObject*>* a_result, std::vector<RE::TESForm*>* downstreamLists = nullptr) {
 		auto forms = a_list->GetContainedForms();
 
 		for (auto* form : forms) {
 			auto* leveledForm = form->As<RE::TESLeveledList>();
 			if (leveledForm) {
-				ResolveContainerLeveledList(leveledForm, a_result);
+				if (!downstreamLists) {
+					auto newBranch = std::vector<RE::TESForm*>();
+					newBranch.push_back(form);
+					ResolveContainerLeveledList(leveledForm, a_result, &newBranch);
+				}
+				else {
+					if (std::find(downstreamLists->begin(), downstreamLists->end(), form) != downstreamLists->end()) {
+						_loggerError("WARNING: Circular Leveled list found. This MUST be resolved, else you will have random crashes and fixes (even without this mod!)");
+						_loggerError("Stack:");
+						for (auto* list : *downstreamLists) {
+							auto edid = clib_util::editorID::get_editorID(list);
+							if (edid.empty()) continue;
+							_loggerError("    >{}", edid);
+						}
+						continue;
+					}
+					downstreamLists->push_back(form);
+					ResolveContainerLeveledList(leveledForm, a_result, downstreamLists);
+				}
+				
 			}
 			else {
 				auto* boundObject = static_cast<RE::TESBoundObject*>(form);
@@ -32,7 +51,6 @@ namespace ContainerCache {
 				if (baseLeveledList) {
 					std::vector<RE::TESBoundObject*> objects{};
 					ResolveContainerLeveledList(baseLeveledList, &objects);
-
 					for (auto* item : objects) {
 						if (this->cachedContainers.contains(container)) {
 							this->cachedContainers[container].push_back(item);
